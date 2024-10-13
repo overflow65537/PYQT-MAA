31990dd651392e412a22dd8e01bd940a1a397f32
overflow <err.overflow@gmail.com>
2024-10-13 20:02:11 +0800

    refactor: 更新至PYQT6

---
+++ app/view/main_window.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from PyQt6.QtGui import QIcon
+from qfluentwidgets import FluentWindow                    
+from qfluentwidgets import FluentIcon as FIF
+from .Task_Interface import TaskInterface
+
+
+
+
+class MainWindow(FluentWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initWindow()
+        # create sub interface
+        self.TaskInterface = TaskInterface(self)
+        # add items to navigation interface
+        self.initNavigation()
+
+    def initNavigation(self):
+        # add navigation items
+        self.addSubInterface(self.TaskInterface, FIF.CHECKBOX,self.tr('每日任务'))
+
+    def initWindow(self):
+        self.resize(960, 780)
+        self.setMinimumWidth(760)
+        self.setWindowIcon(QIcon(':/gallery/images/logo.png'))
+        self.setWindowTitle('PyQt-Fluent-Widgets')
+
+
+
