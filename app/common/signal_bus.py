d2cc21aff47aef3395f3e0f73d1a6275f71c8fea
overflow <err.overflow@gmail.com>
2024-10-24 20:52:10 +0800

    perf: 将所有信号集中存放至signal_bus

--- app/common/signal_bus.py
+++ app/common/signal_bus.py
@@ -7,6 +7,9 @@ class SignalBus(QObject):
     switchToSampleCard = pyqtSignal(str, int)
     micaEnableChanged = pyqtSignal(bool)
     supportSignal = pyqtSignal()
+    adb_detected = pyqtSignal(list)  # 自动查找发送adb设备信息信号
+    update_adb = pyqtSignal(dict)  # 设置界面接收adb设备信息信号
+    callback = pyqtSignal(str)  # 回调协议信号
 
 
 signalBus = SignalBus()
--- app/logic/notification.py
+++ app/logic/notification.py
@@ -1,16 +1,12 @@
 from maa.notification_handler import NotificationHandler, NotificationType
-from PyQt6.QtCore import pyqtSignal, QObject
+from ..common.signal_bus import signalBus
 from datetime import datetime
-
-
-class callbackSignal(QObject):
-    callback = pyqtSignal(str)
 
 
 class MyNotificationHandler(NotificationHandler):
 
     def __init__(self, parent=None):
-        self.callbackSignal = callbackSignal()
+        self.callbackSignal = signalBus
 
     def on_resource_loading(
         self,
@@ -26,22 +22,23 @@ class MyNotificationHandler(NotificationHandler):
     ):
         now_time = datetime.now().strftime("%H:%M:%S")
         if noti_type.value == 1:
-            self.callbackSignal.callback.emit(f"{now_time}"+" 连接中")
+            self.callbackSignal.callback.emit(f"{now_time}" + " 连接中")
         elif noti_type.value == 2:
-            self.callbackSignal.callback.emit(f"{now_time}"+" 连接成功")
+            self.callbackSignal.callback.emit(f"{now_time}" + " 连接成功")
         elif noti_type.value == 3:
-            self.callbackSignal.callback.emit(f"{now_time}"+" 连接失败")
+            self.callbackSignal.callback.emit(f"{now_time}" + " 连接失败")
         else:
-            self.callbackSignal.callback.emit(f"{now_time}"+" 连接状态未知")
+            self.callbackSignal.callback.emit(f"{now_time}" + " 连接状态未知")
 
     def on_tasker_task(
-        self,
-        noti_type: NotificationType,
-        detail: NotificationHandler.TaskerTaskDetail
+        self, noti_type: NotificationType, detail: NotificationHandler.TaskerTaskDetail
     ):
         now_time = datetime.now().strftime("%H:%M:%S")
-        status_map = {0: "未知",  1: "运行中",  2: "成功",  3: "失败"}
+        status_map = {0: "未知", 1: "运行中", 2: "成功", 3: "失败"}
         self.callbackSignal.callback.emit(
-            f"{now_time}" + " " +
-            f"{detail.entry}" + " " +
-            f"{status_map[noti_type.value]}")
+            f"{now_time}"
+            + " "
+            + f"{detail.entry}"
+            + " "
+            + f"{status_map[noti_type.value]}"
+        )
